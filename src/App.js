3bed890c76944d1e7f6f053a44f7407175ad26ab
SKY <worte5633@gmail.com>
2024-07-15 00:42:25 +0900

    240715 metarial3 study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import '@material/web/all.js';
+import {FabSize} from '@material/web/fab/fab.js';
+
+function showDialog(event) {
+  ((event.target).nextElementSibling)?.show();
+}
+
+const icon_Google = <svg viewBox="0 0 24 24">
+      <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+      <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+      <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+      <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+      <path d="M1 1h22v22H1z" fill="none" />
+    </svg>;
+
+
+const standard = (icon, headline, supportingText, quick, noFocusTrap) => {
+    return (
+      <section>
+        
+      <md-filled-button onClick={showDialog} aria-label="Open a dialog">
+        Open
+      </md-filled-button>
+
+      <md-dialog
+        aria-label={headline ? headline : 'A simple dialog'}
+        quick={quick}
+        no-focus-trap={noFocusTrap}>
+        <md-icon slot="icon">{icon}</md-icon>
+        <div slot="headline">{headline}</div>
+        <form id="form" slot="content" method="dialog">
+          <span>{supportingText}</span>
+        </form>
+        <div slot="actions">
+          <md-outlined-button form="form" value="close">Close</md-outlined-button>
+          <md-filled-button form="form" value="ok" autofocus>OK</md-filled-button>
+        </div>
+      </md-dialog>
+      </section>
+    );
+};
+
+const FAB = (label, lowered, size, icon) => {
+  return (
+    <label>
+          <md-fab
+            aria-label={label ? label : 'An example surface FAB'}
+            variant="surface"
+            lowered={lowered}
+            label={label}
+            size={size}>
+            <md-icon slot="icon">{icon}</md-icon>
+          </md-fab>
+        </label>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {standard(icon_Google,"제목","안내글입니당",false,false)}
+      {FAB("라벨", false, "medium", icon_Google)}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,6 @@
+export default function Button (){
+return(
+    <md-outlined-button>Back</md-outlined-button>
+    <md-filled-button>Complete</md-filled-button>
+)
+}
